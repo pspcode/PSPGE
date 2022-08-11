41ad005fafe0ccd741e69004e10bf9f08830afcf
PSP Code <110571905+pspcode@users.noreply.github.com>
2022-08-11 12:47:52 -0300

    scene creator added

---
+++ engine/pspcode/com/scenes/SceneInitializer.java
@@ -0,0 +1,8 @@
+package pspcode.com.scenes;
+
+public abstract class SceneInitializer {
+	
+    public abstract void init(Scene scene);
+    public abstract void loadResources(Scene scene);
+    
+}
