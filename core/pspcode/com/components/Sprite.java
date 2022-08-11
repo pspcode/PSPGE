c0ef28a9a345cce2e442849bbd99663c1af3c06c
PSP Code <110571905+pspcode@users.noreply.github.com>
2022-08-11 14:57:20 -0300

    adding sprite components

---
+++ core/pspcode/com/components/Sprite.java
@@ -0,0 +1,54 @@
+package pspcode.com.components;
+
+import org.joml.Vector2f;
+
+import pspcode.com.renderer.Texture;
+
+public class Sprite {
+
+    private float width, height;
+
+    private Texture texture = null;
+    private Vector2f[] texCoords = {
+                new Vector2f(1, 1),
+                new Vector2f(1, 0),
+                new Vector2f(0, 0),
+                new Vector2f(0, 1)
+        };
+
+    public Texture getTexture() {
+        return this.texture;
+    }
+
+    public Vector2f[] getTexCoords() {
+        return this.texCoords;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setTexture(Texture tex) {
+        this.texture = tex;
+    }
+
+    public void setTexCoords(Vector2f[] texCoords) {
+        this.texCoords = texCoords;
+    }
+
+    public int getTexId() {
+        return texture == null ? -1 : texture.getId();
+    }
+}
---
+++ core/pspcode/com/components/Spritesheet.java
@@ -0,0 +1,58 @@
+package pspcode.com.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joml.Vector2f;
+
+import pspcode.com.renderer.Texture;
+
+public class Spritesheet {
+
+	private Texture texture;
+    private List<Sprite> sprites;
+
+    public Spritesheet(Texture texture, int spriteWidth, int spriteHeight, int numSprites, int spacing) {
+        this.sprites = new ArrayList<>();
+
+        this.texture = texture;
+        int currentX = 0;
+        int currentY = texture.getHeight() - spriteHeight;
+        for (int i = 0; i < numSprites; i++) {
+            float topY = (currentY + spriteHeight) / (float)texture.getHeight();
+            float rightX = (currentX + spriteWidth) / (float)texture.getWidth();
+            float leftX = currentX / (float)texture.getWidth();
+            float bottomY = currentY / (float)texture.getHeight();
+
+            Vector2f[] texCoords = {
+                    new Vector2f(rightX, topY),
+                    new Vector2f(rightX, bottomY),
+                    new Vector2f(leftX, bottomY),
+                    new Vector2f(leftX, topY)
+            };
+            Sprite sprite = new Sprite();
+            sprite.setTexture(this.texture);
+            sprite.setTexCoords(texCoords);
+            sprite.setWidth(spriteWidth);
+            sprite.setHeight(spriteHeight);
+            this.sprites.add(sprite);
+
+            currentX += spriteWidth + spacing;
+            if (currentX >= texture.getWidth()) {
+                currentX = 0;
+                currentY -= spriteHeight + spacing;
+            }
+        }
+    }
+
+    public Sprite getSprite(int index) {
+        return this.sprites.get(index);
+    }
+
+    public List<Sprite> getSprites() { return this.sprites; }
+
+    public int size() {
+        return sprites.size();
+    }
+}
+
