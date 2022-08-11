ad3b35a1c6da8f7852164f978674f142259e7693
PSP Code <110571905+pspcode@users.noreply.github.com>
2022-08-11 11:53:19 -0300

    Adding component system

---
+++ psp/org/pspcode/com/components/Component.java
@@ -0,0 +1,33 @@
+package org.pspcode.com.components;
+
+public abstract class Component {
+	
+    private static int ID_COUNTER = 0;
+    private int uid = -1;
+
+    public transient GameObject gameObject = null;
+
+    public void start() {
+
+    }
+
+    public void update(float dt) {
+
+    }
+
+    public void editorUpdate(float dt) {
+
+    }
+    
+
+    public void destroy() {
+
+    }
+    
+    public void generateId() {
+        if (this.uid == -1) {
+            this.uid = ID_COUNTER++;
+        }
+    }
+
+}
