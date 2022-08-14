714429ad2115eddb1ce810a5c46936f6bfd8c32c
PSP Code <110571905+pspcode@users.noreply.github.com>
2022-08-14 14:37:22 -0300

    Delete engine directory

--- engine/pspcode/com/scenes/SceneInitializer.java
+++
@@ -1,8 +0,0 @@
-package pspcode.com.scenes;
-
-public abstract class SceneInitializer {
-	
-    public abstract void init(Scene scene);
-    public abstract void loadResources(Scene scene);
-    
-}
