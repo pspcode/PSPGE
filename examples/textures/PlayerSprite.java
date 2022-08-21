b303b3eee3c66e612c381463a74e6349cdf7bd74
PSP Code <110571905+pspcode@users.noreply.github.com>
2022-08-21 15:50:06 -0300

    Create PlayerSprite.java

---
+++ examples/textures/PlayerSprite.java
@@ -0,0 +1,15 @@
+class PlayerSprite {
+  // this is a sprite of the player
+  Sprite sprite = new Sprite();
+  
+  public PlayerSprite() {
+    
+   // defines attributes of sprite
+    
+    sprite.setTexture("path/playersprite.png");
+    sprite.setWidth(16);
+    sprite.setHeight(16);
+    
+    
+  }
+}
