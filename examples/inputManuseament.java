0eed8db8bdc5b38cb08e64ff253a6e29ad7cfdc4
PSP Code <110571905+pspcode@users.noreply.github.com>
2022-08-21 15:47:20 -0300

    adding examples of pspcode

---
+++ examples/inputManuseament.java
@@ -0,0 +1,9 @@
+public class InputManuseament {
+  
+  private void onInput() {
+    if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+       System.out.println("The key SPACE is pressed."); 
+    }
+  }
+  
+}
