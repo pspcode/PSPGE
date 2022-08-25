2621c650029a053d86ff37050e975aaa41cb5368
PSP Code <110571905+pspcode@users.noreply.github.com>
2022-08-25 12:10:41 -0300

    creating gamepadhandler example

---
+++ examples/GamepadHandler.java
@@ -0,0 +1,12 @@
+public class GamepadHandler {
+
+  public void doAnimation() {
+    if (GamepadListener.buttonPressed(GLFW_GAMEPAD_BUTTON_X) {
+        // do animation here
+      
+      // .....
+    
+    }
+  }
+     
+} 
